262fd4ffde667592fa2872cae40e6cf5124a25cf
hatef khodkar <hatef.khodkar@hotmail.com>
2024-11-23 17:17:24 +0330

    feat: lazy load lib by route

--- apps/angular-store/src/app/app.routes.ts
+++ apps/angular-store/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: NxWelcomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'orders',
+    loadComponent: () => import('@angular-monorepo/orders').then(m => m.OrdersComponent)
+  }
+];
