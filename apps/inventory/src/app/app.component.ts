7617e2d7471aef7d1fb0b15e5b164cc2f1920c76
hatef khodkar <hatef.khodkar@hotmail.com>
2024-11-23 17:17:48 +0330

    feat: lazy load lib by using @defer

--- apps/inventory/src/app/app.component.ts
+++ apps/inventory/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ProductsComponent } from '@angular-monorepo/products';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule,ProductsComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
